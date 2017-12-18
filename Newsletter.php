6a0c3d511935b1e5254e66b73d94878182fdc849
Danmaguire <daniel.maguire@ucdconnect.ie>
2017-12-18 11:55:13 +0000

    Code

---
+++ Newsletter.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>
+	Contact Received
+</title>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<style>
+
+</style>
+
+</head>
+
+<body>
+
+<div id="header" class="sidecolor">
+	
+	<!--Logo-->
+	<div id="logo">
+	<img src="images/logo1.png" style="width:170px;height:170px;">
+	</div>
+	<!--Links -->
+	<div id="links">	
+		<a href="Contact.html">Contact Us</a>
+		<a href="FAQ.html">FAQ</a>
+		<a href="Sitemap.html">Sitemap</a>
+	</div>
+	<!--Navigation Menu-->
+	<div id="navbar">
+		<nav id="menu">
+			<ul>
+				<li><a href="Homepage.html">Home</a></li>
+				<li><a href="Events.html">Events</a></li>
+				<li><a href="Community.html">Community</a></li>
+				<li><a href="Sports.html">Sports</a></li>
+				<li><a href="About.html">About</a></li>
+				<li><a href="Membership.html">Membership</a></li>
+			</ul>
+		</nav>	
+	</div>
+</div>
+
+
+<div id="homebod">
+<div style="padding-right: 10%;">
+<h1> Thank you for you for joining our mailing list! </h1>
+<?php echo "<h3>".$_POST["mail"]."</h3>";?>
+
+</div>
+</div>
+
+
+<div  id="footer">
+	<!--Subscription form-->
+	<div id="esub">
+		<h1 >JOIN OUR GET FIT, STAY HEALTHY CULTURE.</h1>
+		<h5 >Be kept up to date on upcoming events, promotions and news.</h5>
+		<form action="Newsletter.php" method="post" id="fform">
+		<div id="fborder">
+			<input type="text" name="mail" size="60px" id="ftb" value="Enter your email address">
+			<input id="fbut" type="submit" value="Sign me up!">
+		</div>
+		</form>
+	</div>
+	<!--Social Networks-->
+	<br><br><br><br>
+	<div id="socico">
+		<a href="#" id="fb" ><img src="icons/soc/fb.png"></a>
+		<a href="#" id="gp" ><img src="icons/soc/g+.png"></a>
+		<a href="#" id="in" ><img src="icons/soc/in.png"></a>
+		<a href="#" id="tw" ><img src="icons/soc/tw.png"></a>
+		<a href="#" id="li" ><img src="icons/soc/li.png"></a>
+		
+	</div>
+	<!--Links-->
+	<br>
+	<div id="fr1">
+		<a href="Homepage.html">Home </a>
+		<a href="Events.html">Events</a>
+		<a href="Community.html">Community</a>
+		<a href="Sports.html">Sports</a>
+	<br><br>
+	<div id="fr2">
+		<a href="Contact.html">Contact Us</a>
+		<a href="About">About</a>
+		<a href="FAQ.html">FAQ</a>
+		<a href="Sitemap.html">Sitemap</a>
+	</div>
+	<!--Copyright banner-->
+	<div id="copyban">
+		<br>
+		<p id="cbp">&copy Made up copyright | ALL RIGHTS RESERVED</p>
+	</div>
+
+</div>
+</html>
---
+++ Practice.py
@@ -0,0 +1,8 @@
+ 
+ list = [1,2,3,4,5]
+ print("Whole list - "+list)
+ print("First element -"+list[0])
+ 
+ tuple = 1,2,3,4,5
+ print("Whole tuple - "+tuple)
+ print("First element -"+tuple[0])
